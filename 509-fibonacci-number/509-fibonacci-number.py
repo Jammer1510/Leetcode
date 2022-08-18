1f4d350ccf650d5f1ec849dfb46e8a0ec44241a8
Jammer1510 <76716282+Jammer1510@users.noreply.github.com>
2022-08-18 15:20:41 -0700

    Time: 53 ms (50.41%), Space: 14 MB (9.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,15 +1,19 @@
 class Solution:
+    
+    
+    cache = dict()
+    
     def fib(self, n: int) -> int:
         
-        cache = dict()
         
-        if n in cache:
-            return cache[n]
+        
+        if n in self.cache:
+            return self.cache[n]
         
         if n < 2:
             result = n
         else:
             result = self.fib(n-1) + self.fib(n-2)
             
-        cache[n] = result 
+        self.cache[n] = result 
         return result
