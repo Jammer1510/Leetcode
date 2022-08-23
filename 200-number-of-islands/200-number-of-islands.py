461d49b5992f0a25a77d9e49ebf32178190eb827
Jammer1510 <76716282+Jammer1510@users.noreply.github.com>
2022-08-22 19:20:28 -0700

    Time: 418 ms (69.82%), Space: 16.5 MB (65.51%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -12,11 +12,30 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] =='1':
-                    self.dfs(grid, i, j)
                     count += 1
+                    self.dfs(grid, i, j)
+                    
         
         return count
-                    
+    
+    #bfs
+    def bfs(self,grid,i,j):
+        q = deque([(i,j)])
+        grid[i][j] = '@'
+        
+        while q:
+            grid[i][j] = q.popleft()
+            for delta_i,delta_j in [(-1,0),(1,0),(0,-1),(0,1)]:
+                next_i = i + delta_i
+                next_j = j + delta_j
+                
+                if next_i < 0 or next_j < 0 or next_i >= len(grid) or next_j >= len(grid[0]) or grid[next_i][next_j] == '1':
+                    continue
+                
+                q.append((next_i,next_j))
+                grid[i][j] = '@'
+        
+    #dfs 
     def dfs(self,grid, i, j):            
             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
                 return
