f06c79440a10b820803e015d728c962ceff31fa6
Timmy Lin <76716282+Jammer1510@users.noreply.github.com>
2023-01-25 16:37:16 -0800

    Time: 203 ms (88.43%), Space: 16.8 MB (83.33%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -5,33 +5,27 @@ class Solution(object):
         :rtype: None Do not return anything, modify rooms in-place instead.
         """
         row_count = len(rooms)
-        
         if row_count == 0:
             return 
         
-        Room,Gate = (1 << 31) - 1, 0
-        DIRECTIONS = [(1,0),(-1,0),(0,-1),(0,1)]
-        
+        Directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        ROOM,GATE = (1 << 31) - 1, 0
         col_count = len(rooms[0])
         q = collections.deque()
         
         for row in range(row_count):
             for col in range(col_count):
-                if rooms[row][col] == Gate:
+                if rooms[row][col] == GATE:
                     q.append((row,col))
-
+                    
         while q:
             cur_row,cur_col = q.popleft()
             
-            for dr,dc in DIRECTIONS:
+            for dr,dc in Directions:
                 next_row = cur_row + dr
                 next_col = cur_col + dc
                 
-            
                 if 0 <= next_row < row_count and 0 <= next_col < col_count and \
-                rooms[next_row][next_col] == Room:
-                    rooms[next_row][next_col] = rooms[cur_row][cur_col] + 1                
+                rooms[next_row][next_col] == ROOM:
+                    rooms[next_row][next_col] = rooms[cur_row][cur_col] + 1
                     q.append((next_row,next_col))
-                    
-        
-        
