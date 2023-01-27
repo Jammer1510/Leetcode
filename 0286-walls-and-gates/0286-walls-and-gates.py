98f5bf30ea5004cc5b3044fe5ed7d9dcbcd52417
Timmy Lin <76716282+Jammer1510@users.noreply.github.com>
2023-01-26 16:52:37 -0800

    Time: 250 ms (90.31%), Space: 17.1 MB (66.61%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,28 +3,30 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        row_count = len(rooms)
-        if row_count == 0:
+        row_cnt = len(rooms)
+        
+        if row_cnt == 0:
             return 
         
         Directions = [(1,0),(-1,0),(0,1),(0,-1)]
         ROOM,GATE = (1 << 31) - 1, 0
-        col_count = len(rooms[0])
-        q = collections.deque()
+        col_cnt = len(rooms[0])
+        queue = collections.deque()
         
-        for row in range(row_count):
-            for col in range(col_count):
+        for row in range(row_cnt):
+            for col in range(col_cnt):
                 if rooms[row][col] == GATE:
-                    q.append((row,col))
+                    queue.append((row,col))
                     
-        while q:
-            cur_row,cur_col = q.popleft()
+                    
+        while queue:
+            cur_row,cur_col = queue.popleft()
             
-            for dr,dc in Directions:
+            for dr,dx in Directions:
                 next_row = cur_row + dr
-                next_col = cur_col + dc
+                next_col = cur_col + dx
                 
-                if 0 <= next_row < row_count and 0 <= next_col < col_count and \
-                rooms[next_row][next_col] == ROOM:
+                if 0 <= next_row < row_cnt and 0 <= next_col < col_cnt \
+                and rooms[next_row][next_col] == ROOM:
                     rooms[next_row][next_col] = rooms[cur_row][cur_col] + 1
-                    q.append((next_row,next_col))
+                    queue.append((next_row,next_col))
